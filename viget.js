bbe7c9cae0510789d230874be7cf07c974311ac1
bandarichandana-2926 <bandarichandana96@gmail.com>
2023-11-15 18:55:08 +0530

    first commit

---
+++ viget.js
@@ -0,0 +1,15 @@
+// const menuOpen=document.getElementById("menubar-open");
+// const menuClose=document.getElementById("menubar-close");
+// const toggleMenu=document.querySelector('.toggle-menu');
+// const body=document.querySelector('body')
+// const vigetClient=document.querySelector('.viget-client').querySelectorAll('figure')
+// menuOpen.addEventListener("click",()=>{
+//     toggleMenu.style.visibility='visible'
+//     toggleMenu.style.opacity='1';
+//     body.style.overflowY="hidden";
+// })
+// menuClose.addEventListener("click",(e)=>{
+//     toggleMenu.style.visibility='hidden';
+//     toggleMenu.style.opacity='0';
+//     body.style.overflowY='scroll'
+// })
